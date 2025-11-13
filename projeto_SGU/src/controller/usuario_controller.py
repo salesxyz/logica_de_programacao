0d115f0b8ced851181ee42d98f08b3f76350900a
Victor Sales <vh051208@gmail.com>
2025-11-13 16:31:48 -0300

    Update usuario_controller.py

--- projeto_SGU/src/controller/usuario_controller.py
+++ projeto_SGU/src/controller/usuario_controller.py
@@ -1,5 +1,5 @@
-from projeto_SGU.src.usuario_services import usuario_service
-import pwinput 
+from services import usuario_service
+import pwinput
 
 def gerenciar_usuario(id):
     print('1. Editar Perfil')
@@ -9,13 +9,12 @@ def gerenciar_usuario(id):
         opcao = int(opcao)
         match opcao:
             case 1:
-                usuario = usuario_service.nome_usuario(id)
+                usuario = usuario_service.listar_usuario_id(id)
                 print(f"{'-'*30} Editar o Usuário {usuario}! {'-'*30}")
 
                 novo_nome = input('Digite o novo nome: ').strip().title()
-                nova_senha = pwinput.pwinput('Digite a nova senha: ').strip()
 
-                usuario_service.editar_usuario(id, novo_nome, nova_senha)
+                usuario_service.editar_usuario(id, novo_nome)
 
             case 2:
                 usuario_service.excluir_usuario(id)
