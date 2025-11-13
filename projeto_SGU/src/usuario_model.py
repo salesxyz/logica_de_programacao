183b8f047c68c178e94ccbba04642b29bc82d9bc
salesxyz <vh051208@gmail.com>
2025-11-13 14:13:03 -0300

    Update usuario_model.py
    Fixed indentation and removed unnecessary blank lines.

--- projeto_SGU/src/usuario_model.py
+++ projeto_SGU/src/usuario_model.py
@@ -6,7 +6,7 @@ class Usuario(Base):
    
 
     #Construtor: quando quero usar os atributos para novos valores
-    def __init__(self,nome, email, senha):
+    def __init__(self,nome, email, senha): 
         self.__nome = nome
         self.__email= email
         self.__senha = senha
@@ -35,19 +35,14 @@ class Usuario(Base):
     def senha(self, senha):
         self.__senha = senha
 
-
-
-
+   
     __tablename__ = 'USUARIO'
 
     id = Column (Interger, primary_key=True, autoincrement=True)
     nome = Column(String(120), nullable=False)
     email = Column(String(120), nullable=False, unique=True)
     senha = Column(String(255), nullable=False)
-    
-
      
-    
 
     def gen_senha(self, senha):
         return sha256.hash(senha)
